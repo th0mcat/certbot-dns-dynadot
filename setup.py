3a2750e289d268d1c04f764f03553b6ac54bd6e1
Dmitriy Geels <dmitriy.geels@gmail.com>
2026-08-10 09:16:19 +0200

    version bump to 0.2.0

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name='certbot-dns-dynadot',
-    version='0.1.3',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'certbot>=2.0.0',
